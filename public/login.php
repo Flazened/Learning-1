94cbbb092202167d6b60ae09f9375d193d5d3b54
Flazened <ytmichaelyusliardy@gmail.com>
2026-07-16 03:08:33 +0700

    feats: implement include header

--- public/login.php
+++ public/login.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <tr>
-        <td>Name</td>
-        <td>Email</td>
-        <td>Password</td>
-    </tr>
-</body>
-</html>
+<?php
+$judul = "login";
+include "../includes/header.php";
+
+?>
