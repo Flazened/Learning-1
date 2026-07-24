d105c05a4c5ef474f884efdb61cc8be79a15da86
Flazened <ytmichaelyusliardy88@gmail.com>
2026-07-24 19:30:01 +0700

    lol

--- public/index.php
+++ public/index.php
@@ -7,12 +7,13 @@ include "../includes/header.php";
 
 ?>
 <div class="bg-orange-500 w-full h-16">
-    kotak
+    <h1> My Destinyyyy</h1>
 </div>
 
     <tr class="">
         <td><a href="login.php" class="btn btn-primary" class="">Login</a></td>
         <td><a href="register.php" class="btn btn-secondary">Register</a></td>
+
         <td></td>
     </tr>
 
